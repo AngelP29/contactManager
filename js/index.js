c3f101cef4744922ffa818f3aa6384471bc55b10
oliverfeher19 <oliver.feher19@gmail.com>
2026-05-19 21:41:52 -0400

    added login validation event handling JS function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-//empty for now, created to include in initial push
+const loginSide = document.getElementById("inputs");
+
+loginSide.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(!username || !password){
+        document.getElementById("error-message").textContent = "❌ Please fill in all the fields ❌";
+    } else{
+        document.getElementById("error-message").textContent = "";
+    }
+});
