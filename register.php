2aad0554b4d7fc45a3a5750c350f0ca1f6924514
Dylan Vandenberg <dylanvandenberg217@gmail.com>
2026-05-20 11:52:54 -0400

    Register API

---
+++ register.php
@@ -0,0 +1,63 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT UserID FROM USERS WHERE Login=?");
+		$stmt->bind_param("s", $inData["login"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc() )
+		{
+			returnWithError("User already exists");
+		}
+		else
+		{
+			$stmt = $conn->prepare("INSERT INTO USERS (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+			$stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["login"], $inData["password"]);
+			$stmt->execute();
+
+			$id = $conn->insert_id;
+
+			returnWithInfo($inData["firstName"], $inData["lastName"], $id);
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
